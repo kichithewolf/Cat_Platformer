dec3eab92fa42a4ac0430479df279ead16afe3ec
kichithewolf <michelle.wong.1205@gmail.com>
2016-03-31 10:49:51 -0400

    Added Main Menu and Scene Menu. LoadOnClick script. Mostly recovery and cleanup from mess of commits I had previously.

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; //Newer way of loading things.
+
+public class LoadOnClick : MonoBehaviour
+{
+    //Code modified from https://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/creating-a-scene-menu
+
+    public GameObject loadingImage;
+
+    public void LoadScene(int level)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(level); //Remove deprecated function.
+    }
+}
