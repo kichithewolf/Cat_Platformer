d766dbc055bf4643205b19a45c8031ed3c267a22
kichithewolf <michelle.wong.1205@gmail.com>
2016-04-07 16:42:07 -0400

    Added script for OnCollison actions. Added clumsy "tutorial" using that script.

---
+++ Assets/Scripts/TextCollideTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextCollideTrigger : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("test");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Destroy(other.gameObject);
+        GameObject.Destroy(gameObject);
+        Debug.Log("Hello", gameObject);
+    }
+}
