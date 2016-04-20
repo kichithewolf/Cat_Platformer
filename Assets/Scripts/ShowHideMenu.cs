21a17ccf304caf708e359761219fad1780c4c029
kichithewolf <michelle.wong.1205@gmail.com>
2016-04-20 18:26:30 -0400

    Added Pause Menu, UI. ("Level 1" only)

---
+++ Assets/Scripts/ShowHideMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHideMenu : MonoBehaviour {
+    public GameObject menuName;
+    private bool isMenuShowing;
+
+    // Use this for initialization
+    void Start () {
+        isMenuShowing = false;
+        menuName.SetActive(isMenuShowing);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
+            else
+            {
+                Time.timeScale = 0;
+            }
+
+            isMenuShowing = !isMenuShowing;
+            menuName.SetActive(isMenuShowing);
+
+        }
+    }
+}
--- Assets/Scripts/TextCollideTrigger.cs
+++ Assets/Scripts/TextCollideTrigger.cs
@@ -13,4 +13,30 @@ public class TextCollideTrigger : MonoBehaviour
         GameObject.Destroy(gameObject);
         //Debug.Log("Hello", gameObject);
     }
-}
+
+    /*public float lifetime = 2;
+    Color fade;
+    float timer;
+    //http://forum.unity3d.com/threads/fading-out-alpha-on-gameobject.371899/
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= lifetime)
+        {
+            //Transparency.
+            fade = GetComponent<Renderer>().material.color;
+            fade.a = fade.a / 1.1f;
+            GetComponent<Renderer>().material.color = fade;
+            //kill when faded
+            if (fade.a <= .1)
+            {
+                Destroy(transform.parent.gameObject);
+                Destroy(gameObject);
+            }
+
+        }
+    }
+    */
+
+    }
