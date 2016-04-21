caee7c292a936d20271bd7148859d68d11f26168
kichithewolf <michelle.wong.1205@gmail.com>
2016-04-21 16:51:20 -0400

    Added countdown timer and game over screen.

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour {
+    [SerializeField]
+    float timeAlloted;
+
+    [SerializeField]
+    int gameOverScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeAlloted -= Time.deltaTime;
+        if(timeAlloted <= 0)
+        {
+            SceneManager.LoadScene(gameOverScene);
+        }
+	}
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(850, 10, 100, 20), timeAlloted.ToString());
+        //GUILayout.Label("Time Remaining: " + timeAlloted);
+    }
+}
