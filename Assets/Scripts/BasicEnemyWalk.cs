c9b2c601ac8094de090f61d6905209628f151969
Michelle Wong <michelle.wong.1205@gmail.com>
2016-04-26 21:41:30 -0400

    Added Basic Enemy Movement.

--- Assets/Scripts/BasicEnemyWalk.cs
+++ Assets/Scripts/BasicEnemyWalk.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class BasicEnemyWalk : MonoBehaviour {
+    // http://answers.unity3d.com/questions/803920/how-to-move-an-object-backforth-on-a-set-path.html
+    public float speed;
+    public float distance;
+    private float xStartPosition;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        xStartPosition = transform.position.x;
+    }
+    void Update()
+    {
+        if ((speed < 0 && transform.position.x < xStartPosition) || (speed > 0 && transform.position.x > xStartPosition + distance))
+        {
+            speed *= -1;
+        }
+        transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+    }
 }
