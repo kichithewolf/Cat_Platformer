caffa1eeeb8ed2fea7fd81452f9fe7ebad9350d8
kichithewolf <michelle.wong.1205@gmail.com>
2016-04-28 16:40:32 -0400

    Enemy Placements and some Background music.

---
+++ Assets/WalkAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkAudio : MonoBehaviour {
+    AudioSource walkingSound;
+
+    void Start()
+    {
+        //walkingSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            walkingSound.Play();
+        }
+        else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            walkingSound.Stop();
+        }
+    }
+}
