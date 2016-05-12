62428ed37d65a4c70fd74276fc97420a260112e9
kichithewolf <michelle.wong.1205@gmail.com>
2016-05-11 21:51:34 -0400

    Added Intro Cutscene (no audio, needs edit), fix to title logo.

---
+++ Assets/PlayMovieOnSpace.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayMovieOnSpace : MonoBehaviour
+{
+    [SerializeField]
+    int nextLevel;
+
+    void Start()
+    {
+        MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        AudioSource audio = GetComponent<AudioSource>();
+        movie.Play();
+        audio.Play();
+        //SceneManager.LoadScene(nextLevel);
+
+    }
+}
