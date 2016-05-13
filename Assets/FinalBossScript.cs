627e274062c0bed4d3a15684938e58bce45ce171
kichithewolf <michelle.wong.1205@gmail.com>
2016-05-12 23:54:41 -0400

    Boss Level Complete

---
+++ Assets/FinalBossScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalBossScript : MonoBehaviour {
+    int speed = 10;
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+    }
+}
