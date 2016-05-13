bb16cef9b6c683fe72b39cfd98ac0acb0adc9c6c
kichithewolf <michelle.wong.1205@gmail.com>
2016-05-12 22:32:57 -0400

    Level 3 Complete. Added Following Enemy (Shark). Added cutscene for Boss level.

---
+++ Assets/Scripts/BasicEnemyCircle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyCircle : MonoBehaviour
+{
+    public float radius = 10f;
+    public float speed = 1f;
+    public bool offsetIsCenter = true;
+    public Vector3 offset;
+
+    void Start()
+    {
+        if (offsetIsCenter)
+        {
+            offset = transform.position;
+        }
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(
+                    (radius * Mathf.Cos(Time.time * speed)) + offset.x,
+                    (radius * Mathf.Sin(Time.time * speed)) + offset.y,
+                    offset.z);
+    }
+}
---
+++ Assets/Scripts/BasicEnemyFollowC.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyFollowC : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 2f;
+    private float minDistance = 1f;
+    private float range;
+    void Update()
+    {
+        range = Vector2.Distance(transform.position, target.position);
+
+        if (range > minDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
